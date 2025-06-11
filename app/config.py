8dfd3147a5a04d4ee5e1333f9d0fbd528b07b1bc
wrapperfoil <wrapperfoil@users.noreply.github.com>
2025-06-11 05:27:37 +0300

    Updated config.py with dotenv

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    DB_SERVER = os.environ.get('DB_SERVER')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'abcde'
+    DB_SERVER = os.environ.get('DB_SERVER') or 'localhost'
