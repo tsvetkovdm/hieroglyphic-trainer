8bf86bbe7e7e6d3321bfb32c01780a2cad61a2bd
tsvetkovdm <tsvetkovdm@users.noreply.github.com>
2025-06-11 16:23:36 +0300

    Updated config.py and routes.py with psycopg connection to db

--- app/config.py
+++ app/config.py
@@ -6,3 +6,6 @@ load_dotenv()
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'abcde'
     DB_SERVER = os.environ.get('DB_SERVER') or 'localhost'
+    DB_USER=os.environ.get('DB_USER')
+    DB_PASSWORD=os.environ.get('DB_PASSWORD')
+    DB_NAME=os.environ.get('DB_NAME')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,24 @@
 from app import app
+import psycopg
 
 @app.route('/')
 def index():
     return "Hi, do you wanna learn some chinese characters?:/"
+
+@app.route('/testdb')
+def test_connection():
+    con = None
+    message = ""
+    try:
+        con = psycopg.connect(host=app.config['DB_SERVER'],
+                              user=app.connect['DB_USER'],
+                              password=app.connect['DB_PASSWORD'],
+                              dbname=app.config['DB_NAME'])
+    except Exception as e:
+        message = f"Ошибка подключения: {e}"
+    else:
+        message = "Подключение успешно"
+    finally:
+        if con:
+            con.close()
+        return message
