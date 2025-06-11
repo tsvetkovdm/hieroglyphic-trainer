ce8a3209056aa89784278e7fd7fdc95392a9a239
tsvetkovdm <tsvetkovdm@users.noreply.github.com>
2025-06-11 21:40:55 +0300

    fixed: csv encoding, added radicals.html and numbers-quatities route

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from flask import render_template
 import psycopg
 
 @app.route('/')
@@ -22,3 +23,16 @@ def test_connection():
         if con:
             con.close()
         return message
+
+@app.route('/numbers-quantities')
+def get_num_quan_group():
+    rad_group = 'numbers-quantities'
+    with psycopg.connect(host=app.config['DB_SERVER'],
+                         user=app.config['DB_USER'],
+                         password=app.config['DB_PASSWORD'],
+                         dbname=app.config['DB_NAME']) as con:
+        cur=con.cursor()
+        rad_symbols = cur.execute(f'SELECT lo.symbol, lo.pinyin, lo.meaning, rg.name, rg.slug FROM "learning_object" AS lo INNER JOIN "radical_group" AS rg ON rg.id = lo.group_id WHERE rg.slug LIKE %s', 
+                                  (rad_group,)).fetchall()
+        return render_template('radicals.html', title='Радикалы группы "Числа и количество"',
+                               rad_symbols=rad_symbols)
--- db/scripts/insert.sql
+++ db/scripts/insert.sql
@@ -1,8 +1,8 @@
-\copy "role"("name") FROM 'C:/projects/hieroglyphic-trainer/db/csv/role.csv' DELIMITER ';' CSV HEADER;
-\copy "training_mode"("name", "access_level", "description") FROM 'C:/projects/hieroglyphic-trainer/db/csv/training_mode.csv' DELIMITER ';' CSV HEADER;
-\copy "radical_group"("name", "slug", "description") FROM 'C:/projects/hieroglyphic-trainer/db/csv/radical_group.csv' DELIMITER ';' CSV HEADER;
-\copy "user"("username", "password", "first_name", "last_name", "email", "date_of_birth", "role_id") FROM 'C:/projects/hieroglyphic-trainer/db/csv/user.csv' DELIMITER ';' CSV HEADER;
-\copy "training_session"("user_id", "mode_id", "date_started", "date_ended", "result") FROM 'C:/projects/hieroglyphic-trainer/db/csv/training_session.csv' DELIMITER ';' CSV HEADER;
-\copy "learning_object"("symbol", "pinyin", "meaning", "stroke_order_image_url", "description", "slug", "group_id") FROM 'C:/projects/hieroglyphic-trainer/db/csv/learning_object.csv' DELIMITER ';' CSV HEADER;
-\copy "training_item"("session_id", "object_id") FROM 'C:/projects/hieroglyphic-trainer/db/csv/training_item.csv' DELIMITER ';' CSV HEADER;
-\copy "user_selection"("user_id", "object_id", "saved_name") FROM 'C:/projects/hieroglyphic-trainer/db/csv/user_selection.csv' DELIMITER ';' CSV HEADER;
+\copy "role"("name") FROM 'C:/projects/hieroglyphic-trainer/db/csv/role.csv' DELIMITER ';' CSV HEADER ENCODING 'UTF8';
+\copy "training_mode"("name", "access_level", "description") FROM 'C:/projects/hieroglyphic-trainer/db/csv/training_mode.csv' DELIMITER ';' CSV HEADER ENCODING 'UTF8';
+\copy "radical_group"("name", "slug", "description") FROM 'C:/projects/hieroglyphic-trainer/db/csv/radical_group.csv' DELIMITER ';' CSV HEADER ENCODING 'UTF8';
+\copy "user"("username", "password", "first_name", "last_name", "email", "date_of_birth", "role_id") FROM 'C:/projects/hieroglyphic-trainer/db/csv/user.csv' DELIMITER ';' CSV HEADER ENCODING 'UTF8';
+\copy "training_session"("user_id", "mode_id", "date_started", "date_ended", "result") FROM 'C:/projects/hieroglyphic-trainer/db/csv/training_session.csv' DELIMITER ';' CSV HEADER ENCODING 'UTF8';
+\copy "learning_object"("symbol", "pinyin", "meaning", "stroke_order_image_url", "description", "slug", "group_id") FROM 'C:/projects/hieroglyphic-trainer/db/csv/learning_object.csv' DELIMITER ';' CSV HEADER ENCODING 'UTF8';
+\copy "training_item"("session_id", "object_id") FROM 'C:/projects/hieroglyphic-trainer/db/csv/training_item.csv' DELIMITER ';' CSV HEADER ENCODING 'UTF8';
+\copy "user_selection"("user_id", "object_id", "saved_name") FROM 'C:/projects/hieroglyphic-trainer/db/csv/user_selection.csv' DELIMITER ';' CSV HEADER ENCODING 'UTF8';
