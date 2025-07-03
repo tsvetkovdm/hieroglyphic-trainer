9366f9b36cd4a1cddb052bd308b3288481588ee5
tsvetkovdm <tsvetkovdm@users.noreply.github.com>
2025-07-03 03:15:24 +0300

    refactor: client encoding utf8 in db connection

--- app/db.py
+++ app/db.py
@@ -5,4 +5,5 @@ def get_connection():
     return psycopg.connect(host=app.config['DB_SERVER'],
                            user=app.config['DB_USER'],
                            password=app.config['DB_PASSWORD'],
-                           dbname=app.config['DB_NAME'])
+                           dbname=app.config['DB_NAME'],
+                           client_encoding='UTF-8')
