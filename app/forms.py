279e14a63bd45a4df98cb65a9ce0d04bb5512c38
tsvetkovdm <tsvetkovdm@users.noreply.github.com>
2025-07-03 03:19:32 +0300

    feat(forms): add AdminCreateUserForm for creating users in admin panel

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, DateField, PasswordField, SelectField, SubmitField, ValidationError, validators
+from wtforms.validators import DataRequired, Email, Length
 from datetime import date
 
 def validate_birth(form, field):
@@ -18,6 +19,17 @@ class RegistrationForm(FlaskForm):
     date_of_birth = DateField('Дата рождения', format='%Y-%m-%d', validators=[validators.InputRequired(), validate_birth])
     want_spam = BooleanField('Я согласен(-а) получать рекламную рассылку')
     submit = SubmitField('Зарегистрироваться')
+
+class AdminCreateUserForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired(), Length(min=3, max=64)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=4)])
+    first_name = StringField('Имя')
+    last_name = StringField('Фамилия')
+    date_of_birth = DateField('Дата рождения')
+    want_spam = BooleanField('Отправлять новости?')
+    role_id = SelectField('Роль', coerce=int)  # Наполнишь в route
+    submit = SubmitField('Создать пользователя')
 
 class LoginForm(FlaskForm):
     username = StringField('Логин', [validators.InputRequired()])
