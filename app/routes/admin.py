a581bd392a4cca2348c03c4a459f9115d069923d
tsvetkovdm <tsvetkovdm@users.noreply.github.com>
2025-07-03 03:11:27 +0300

    feat: adding a new user in the admin panel

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,11 +1,13 @@
+from werkzeug.security import generate_password_hash
 from flask import Blueprint, render_template, redirect, flash, url_for, request, abort
 from flask_login import current_user
-from app.routes import admin
-from app.forms import EditUserForm
+from app.forms import EditUserForm, AdminCreateUserForm
 from app.db import get_connection
 from functools import wraps
 
 admin = Blueprint('admin', __name__)
+
+DEFAULT_ROLE_ID = 2
 
 def admin_required(f):
     @wraps(f)
@@ -17,7 +19,7 @@ def admin_required(f):
 
 @admin.route('/')
 @admin_required
-def admin_dashboard():
+def dashboard():
     with get_connection() as conn:
         cur = conn.cursor()
         cur.execute('''
@@ -27,11 +29,46 @@ def admin_dashboard():
                     ON u.role_id = r.id 
                     ''')
         users = cur.fetchall()
-    return render_template('admin/admin_dashboard.html', users=users)
+    return render_template('admin/dashboard.html', users=users)
+
+@admin.route('/user/add/', methods=['GET', 'POST'])
+@admin_required
+def add_user():
+    form = AdminCreateUserForm()
+    with get_connection() as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT id, name FROM "role"')
+        roles = cur.fetchall()
+        form.role_id.choices = [(r[0], r[1]) for r in roles]
+
+        if form.validate_on_submit():
+            cur.execute('SELECT id FROM "user" WHERE username = %s OR email = %s',
+                        (form.username.data, form.email.data))
+            if cur.fetchone():
+                flash("Пользователь с таким именем или email уже существует", "danger")
+                return redirect(url_for('admin.add_user'))
+
+            password_hash = generate_password_hash(form.password.data)
+            cur.execute('''
+                        INSERT INTO "user" 
+                        (username, password, first_name, last_name, 
+                        email, date_of_birth, want_spam, role_id) 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                        ''',
+                        (form.username.data, password_hash, 
+                         form.first_name.data, form.last_name.data, 
+                         form.email.data, form.date_of_birth.data, 
+                         form.want_spam.data, form.role_id.data))
+            conn.commit()
+
+            flash(f'Пользователь {form.username.data} успешно создан', 'success')
+            return redirect(url_for('admin.dashboard'))
+
+    return render_template('admin/add_user.html', form=form, title=f'Создание пользователя')
 
 @admin.route('/user/edit/<int:user_id>', methods=['GET', 'POST'])
 @admin_required
-def admin_edit_user(user_id):
+def edit_user(user_id):
     form = EditUserForm()
     with get_connection() as conn:
         cur = conn.cursor()
@@ -70,24 +107,24 @@ def admin_edit_user(user_id):
                          user_id))
             conn.commit()
             flash('Пользователь обновлён', 'success')
-            return redirect(url_for('admin_dashboard'))
+            return redirect(url_for('admin.dashboard'))
 
     return render_template('profile.html', form=form, title=f'Редактировать профиль {user_data[0]}', show_role = True)
 
 @admin.route('/user/delete/<int:user_id>', methods=['POST'])
 @admin_required
-def admin_delete_user(user_id):
+def delete_user(user_id):
     if user_id == current_user.id:
         flash('Вы не можете удалить сами себя.', 'warning')
-        return redirect(url_for('admin_dashboard'))
+        return redirect(url_for('admin.dashboard'))
 
     with get_connection() as conn:
         cur = conn.cursor()
         cur.execute('SELECT id FROM "user" WHERE id = %s', (user_id,))
         if cur.fetchone() is None:
             flash('Пользователь не найден.', 'danger')
-            return redirect(url_for('admin_dashboard'))
+            return redirect(url_for('admin.dashboard'))
 
         cur.execute('DELETE FROM "user" WHERE id = %s', (user_id,))
         flash('Пользователь удалён.', 'success')
-        return redirect(url_for('admin_dashboard'))
+        return redirect(url_for('admin.dashboard'))
